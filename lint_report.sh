b6f07b4b8130bfaa8e32d949e41a6947ca1cb125
Cindy Lee <cindylee0113@gmail.com>
2019-05-02 19:36:51 -0400

    Reporting no errors for the files that don't have linting errors

--- lint_report.sh
+++ lint_report.sh
@@ -4,5 +4,11 @@ PYTHON_FILES=$(find ../NYCOpenRecords -name '*.py');
 
 for filename in $PYTHON_FILES; do
     LINT_FILE_NAME=templates/$(echo ${filename##'../NYCOpenRecords/'} | sed -e 's,/,_,g' | sed -e 's/.py/.py_lint.txt/')
-    $(exec flake8 --max-line-length 120 $filename \; | sed -e 's,../NYCOpenRecords/,,g' > $LINT_FILE_NAME)
+    REPORT=$(exec flake8 --max-line-length 120 $filename \; | sed -e 's,../NYCOpenRecords/,,g')
+    if [ "$REPORT"  == "" ]; then
+        $(echo "No problems to report" > $LINT_FILE_NAME)
+    else
+        $(echo "$REPORT" > $LINT_FILE_NAME)
+    fi
+    # $(cat $LINT_FILE_NAME)
 done

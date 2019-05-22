3bdc753ebaa5fb304d14c43a9988a5321c6e902c
Cindy Lee <cindylee0113@gmail.com>
2019-05-21 21:43:33 -0400

    Testing relative paths for the github pages

--- csv_file_names.py
+++ csv_file_names.py
@@ -53,8 +53,8 @@ def create_csv(connector):
     header = connector.join(header)
     base_url = "/NYCOpenDocs/"
     # html_url = base_url + "html/"
-    html_url = "./html/"
-    home = ["1", "Home", base_url + "index.html", "", "glyphicon-home"]
+    html_url = "/html/"
+    home = ["1", "Home", "index.html", "", "glyphicon-home"]
     home = connector.join(home)
     output.append(header)
     output.append(home)

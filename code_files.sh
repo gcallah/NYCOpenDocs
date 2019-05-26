6825f3eb683e5120e3a7d28958e6e3c987d37422
Cindy Lee <cindylee0113@gmail.com>
2019-05-26 03:12:45 -0400

    Adding the data folder to navbar and JSON files to extension list

--- code_files.sh
+++ code_files.sh
@@ -3,8 +3,6 @@ if [ -z path ]; then
     path="."
 fi
 echo $path
-# find $path -maxdepth 1 -o -regex ".*\.\(py\|css\|html\)"
-# find $path -type f \( -name '*.py' -or -name '*.js' -or -name '*.yml' -or -name '*.sh' -or -name '*.html' -or -name '*.css' \)
 
 EXTENSION_FILE=$2
 ext=''

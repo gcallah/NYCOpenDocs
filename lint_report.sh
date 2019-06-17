4cfcc4e2d0cae9a282aae76110a197a825ea7a7b
Cindy Lee <cindylee0113@gmail.com>
2019-06-16 20:29:30 -0400

    Edited linting script to note that linting is not performed on minified files

--- lint_report.sh
+++ lint_report.sh
@@ -8,6 +8,7 @@ fi
 PYTHON_FILES=$(find ${OPEN_RECORDS_REPO} -name '*.py');
 JS_FILES=$(find ${OPEN_RECORDS_REPO} -name '*.js' ! -name '*.min.js')
 CSS_FILES=$(find ${OPEN_RECORDS_REPO} -name '*.css' ! -name '*.min.css')
+MIN_FILES=$(find ${OPEN_RECORDS_REPO} -name '*.min.js' -o -name '*.min.css')
 
 for filename in $PYTHON_FILES; do
     LINT_FILE_NAME=templates/$(echo ${filename##$OPEN_RECORDS_REPO'/'} | sed -e 's,/,_,g' | sed -e 's/.py/.py_lint.txt/')
@@ -36,4 +37,10 @@ for filename in $CSS_FILES; do
     fullPath=${PWD:0:$length-11}NYCOpenRecords/${filename##$OPEN_RECORDS_REPO'/'} 
     REPORT=$(npx csslint ${filename} | sed -e "s,$fullPath,,g")
     $(echo "$REPORT" > $LINT_FILE_NAME)
+done
+
+for filename in $MIN_FILES; do
+    LINT_FILE_NAME=templates/$(echo ${filename##$OPEN_RECORDS_REPO'/'} | sed -e 's,/,_,g' | sed -e 's/.js$/.js_lint.txt/' |  sed -e 's/.css$/.css_lint.txt/')
+    echo $LINT_FILE_NAME
+    $(echo "Linting is not performed on minified files." > $LINT_FILE_NAME)
 done

c81d7c4033b1579ce1c0b3644aed8d531f644e74
Cindy Lee <cindylee0113@gmail.com>
2019-06-17 02:36:03 -0400

    Modifying eslintrc to handle jquery

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,10 @@ module.exports = {
         "browser": true,
         "es6": true,
         "node": true,
+        "jquery": true,
         // "mocha": true,
     },
-    "extends": "eslint:recommended",
+    "extends": ["eslint:recommended", "jquery"],
     "parserOptions": {
         "ecmaVersion": 8
     },
